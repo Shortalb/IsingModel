69e06d149db48d0de8d95a2e6368396d07b64533
Brian <shortalb@tcd.ie>
2018-01-12 20:13:19 +0000

    Main .py updated to include lattice creator and get energy change as it wasnt working without. also added are functions to test several observables. MAG SUSC TEST not yet working.

--- ModulatedCode/main.py
+++ ModulatedCode/main.py
@@ -2,23 +2,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import totalenergy as Etotal
-import getenergychange as Eget
-import latticecreator as LatticeGet
+
+
+import meanenergy as Emean
 import getmeanmag as Mget
-import meanesquared as Esquared
+#import meanesquared as Esquared
 import meanmagsquared as Msquared
-import Cv as cv
-import magsusc
-T = 2.0 #temp multiplied by kB
-n = 128 # matrix size
+#import Cv as cv
+import magsusc as magsusc
+T = 0.00001 #temp multiplied by kB
+n = 50 # matrix size
 h = 0.0 #external magnetic field value
 
 
-lattice = LatticeGet.lattice_creator(n) #globalise the name lattice
-mean_energy_squared = Esquared.get_mean_energy_squared(lattice)
-mean_energy = (Etotal.get_total_energy(lattice))/n**2
-mean_mag_squared = Msquared.get_mean_mag_squared(lattice)
 
+
+
+
+def lattice_creator(n): #new function to make lattice
+        lattice = np.random.choice([-1,-1], [n,n]) 
+        return lattice
+lattice = lattice_creator(n)
+
+def get_energy_change(h,i,j):
+    
+    #periodic boundary conditions
+    top = lattice[(i-1)%n][j] 
+    bottom = lattice[(i+1)%n][j] 
+    left = lattice[i][(j-1)%n] 
+    right = lattice[i][(j+1)%n]
+    
+    #defining the hamiltonian 
+    net_spin = top + bottom + left + right
+    chosen_spin = lattice[i][j]
+    energy_change = -2*chosen_spin*(net_spin+h)
+    
+    return energy_change
 
 def spin_flipper(T): #function to select and flip a random spin
 
@@ -28,7 +47,7 @@ def spin_flipper(T): #function to select and flip a random spin
     r = np.random.random()
 
     #calling get energy change and assigning name to result
-    energy_change = Eget.get_energy_change(h,i,j)
+    energy_change = get_energy_change(h,i,j)
 
     #defining the finite probability flip clause
     flip_clause = np.exp(energy_change/(T))
@@ -45,28 +64,41 @@ def spin_flipper(T): #function to select and flip a random spin
     return lattice
 
 
-print Etotal.get_total_energy(lattice)
+
+
+
 def graph_equilibrium_test():
 	mean_mag_list = []
-	steps = range(1000)
-	for iteration in steps:
-		spin_flipper(T)
+	sweep_size = 1000
+	n_steps = 1000
+
+	for i in range(n_steps):
+		for iteration in range(sweep_size):
+			spin_flipper(T)
 		mean_magnetisation = Mget.get_mean_magnetisation(lattice)
 		mean_mag_list.append(mean_magnetisation)
-	plt.plot(steps, mean_mag_list)
-######add labels#####
-	plt.show()
 
+	
+	plt.plot(range(n_steps), mean_mag_list)
+	plt.title("Equilibrium Test for 50x50 Lattice")
+	plt.xlabel("Number of Steps")
+	plt.ylabel("Mean Magnetisation Per Spin")
+	plt.grid(True)
+	plt.show()
+#graph_equilibrium_test()
+####critical temperature test ####
 def get_crit_temp_graph():
+
 	mean_mag_list= []
 	temps = np.arange(0.05, 5.0,0.05)
 
 	for temp in temps:
-		print temp
-		for iteration in range(50):
+		for iteration in range(100000):
 			spin_flipper(temp)
-		mean_mag = Mget.get_mean_magnetisation(lattice)
-		mean_mag_list.append(mean_mag)
+		mean_magnetisation = Mget.get_mean_magnetisation(lattice)
+		mean_mag_list.append(mean_magnetisation)
+		print temp
+
 
 	plt.plot(temps, mean_mag_list)
 	plt.title("Plot showing critical temperature")
@@ -74,4 +106,28 @@ def get_crit_temp_graph():
 	plt.ylabel("Mean magnetisation per spin")
 	plt.grid(True)
 	plt.show()
-get_crit_temp_graph()
+#get_crit_temp_graph()
+mean_magnetisation = Mget.get_mean_magnetisation(lattice)
+mean_mag_squared = Msquared.get_mean_mag_squared(lattice)
+#########magnetic susc graph####
+def get_mag_susc_graph():
+	mag_susc_list = []
+	temps = np.arange(0.05,5.0,0.05)
+	mag_susc = 0
+	for temp in temps:
+		for iteration in range(100000):
+			spin_flipper(temp)
+		mag_susc = mag_susc.get_magnetic_susc(mean_mag_squared, mean_magnetisation)
+		mag_susc_list.append(mag_susc)
+		print mag_susc
+
+	plt.plot(temps, mag_susc_list)
+	plt.title("Plot Showing Magnetic Susceptibility vs Temperature")
+	plt.ylabel("Magnetic Susceptibility")
+	plt.xlabel("Temperature")
+	plt.grid(True)
+	plot.show()
+get_mag_susc_graph()
+
+
+

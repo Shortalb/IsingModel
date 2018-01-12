f41867e8c787e91596a6e3b9ba21b2209d50b0a5
Brian <shortalb@tcd.ie>
2018-01-12 13:16:51 +0000

    error in the i and j range. was counting one less row/column than existed

--- ModulatedCode/getmeanmag.py
+++ ModulatedCode/getmeanmag.py
@@ -5,17 +5,15 @@ h = 0.0
 import getenergychange as Eget
 import numpy as np
 import latticecreator as LatticeGet
-   
+
 
 #####mean magnetisation per spin
 
 def get_mean_magnetisation(lattice):
 	total_magnetisation = float(0)
 
-	for i in range(0,n-1):
-		for j in range(0,n-1):
+	for i in range(0,n):
+		for j in range(0,n):
 			total_magnetisation += lattice[i][j]
 	mean_magnetisation = total_magnetisation/n**2
 	return mean_magnetisation
-
-
